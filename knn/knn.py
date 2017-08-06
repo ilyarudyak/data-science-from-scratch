10ba3d4fc516adca58517b9e994b5caa858f3593
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-08-06 21:33:45 +0300

    done with kNN

--- knn/knn.py
+++ knn/knn.py
@@ -8,6 +8,14 @@ import matplotlib.pyplot as plt
 import csv
 
 
+def get_cities(filename):
+    with open(filename) as f:
+        f_csv = csv.reader(f)
+        headings = next(f_csv)
+        City = namedtuple('City', headings)
+        return [City(float(r[0]), float(r[1]), r[2]) for r in f_csv]
+cities = get_cities('cities.csv')
+
 def raw_majority_vote(labels):
     votes = Counter(labels)
     winner, _ = votes.most_common(1)[0]
@@ -53,12 +61,14 @@ def knn_classify(k, labeled_points, new_point):
     return majority_vote(k_nearest_labels)
 
 
-def get_cities(filename):
-    with open(filename) as f:
-        f_csv = csv.reader(f)
-        headings = next(f_csv)
-        City = namedtuple('City', headings)
-        return [City(*r) for r in f_csv]
+def knn_classify2(k, cities, new_point):
+    by_distance = sorted(cities,
+                         key=lambda city:
+                         distance((city.longitude, city.latitude), new_point))
+
+    k_nearest_labels = [label for _, _, label in by_distance[:k]]
+
+    return majority_vote(k_nearest_labels)
 
 
 def plot_cities():
@@ -86,5 +96,49 @@ def plot_cities():
     plt.show()
 
 
+def classify_and_plot_grid(k=1):
+    plots = {"Java": ([], []), "Python": ([], []), "R": ([], [])}
+    markers = {"Java": "o", "Python": "s", "R": "^"}
+    colors = {"Java": "r", "Python": "b", "R": "g"}
+
+    for longitude in range(-130, -60):
+        for latitude in range(20, 55):
+            predicted_language = knn_classify2(k, cities, [longitude, latitude])
+            plots[predicted_language][0].append(longitude)
+            plots[predicted_language][1].append(latitude)
+
+    # create a scatter series for each language
+    for language, (x, y) in plots.items():
+        plt.scatter(x, y, color=colors[language], marker=markers[language],
+                    label=language, zorder=0)
+
+    plot_state_borders()  # assume we have a function that does this
+
+    plt.legend(loc=0)  # let matplotlib choose the location
+    plt.axis([-130, -60, 20, 55])  # set the axes
+    plt.title(str(k) + "-Nearest Neighbor Programming Languages")
+    plt.show()
+
+
+def choose_k():
+    cities = get_cities('cities.csv')
+
+    for k in [1, 3, 5, 7]:
+        num_correct = 0
+
+        for longitude, latitude, actual_language in cities:
+
+            other_cities = [other_city
+                            for other_city in cities
+                            if other_city != (longitude, latitude, actual_language)]
+
+            predicted_language = knn_classify2(k, other_cities, [longitude, latitude])
+
+            if predicted_language == actual_language:
+                num_correct += 1
+
+        print(k, "neighbor[s]:", num_correct, "correct out of", len(cities))
+
+
 if __name__ == '__main__':
-    plot_cities()
+    classify_and_plot_grid(k=1)
