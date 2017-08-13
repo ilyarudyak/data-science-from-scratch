80e6853168477be56b6c7d49ff2f3832c4b004b6
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-08-13 15:51:06 +0300

    more on tennis set

--- decision-trees/tennis.py
+++ decision-trees/tennis.py
@@ -9,34 +9,53 @@ def get_tennis_data():
         f_csv = csv.reader(f)
         headings = next(f_csv)
         Tennis = namedtuple('Tennis', headings)
-        for r in f_csv:
-            tennis_data.append(Tennis(*r))
-            # print('{:3s} {:8s} {:4s} {:6s} {:6s} {}'.format(*r))
+        for row in f_csv:
+            tennis_data.append(Tennis(*row))
+            # print('{:3s} {:8s} {:4s} {:6s} {:6s} {:3s}'.format(*r))
     return tennis_data
+
+
+def get_first_level_outcomes(tennis_data):
+    return {heading: count_outcomes(tennis_data, heading) for heading in tennis_data[0]._fields[1:-1]}
+
+
+def count_outcomes(tennis_data, heading):
+    outcomes = defaultdict(lambda: [0, 0, 0])
+    for tennis_row in tennis_data:
+        outcomes[getattr(tennis_row, heading)][0 if tennis_row.play_tennis == 'yes' else 1] += 1
+
+    for outcome in outcomes:
+        total = outcomes[outcome][0] + outcomes[outcome][1]
+        outcomes[outcome][0] /= total
+        outcomes[outcome][1] /= total
+        outcomes[outcome][2] = total
+
+    return dict(outcomes)
+
+
+def get_first_level_gains(tennis_data):
+    entropy0, total_labels = get_zero_level_entropy(tennis_data)
+    outcomes = get_first_level_outcomes(tennis_data)
+    gains = {}
+    for heading in outcomes:
+        # print('{}:'.format(heading))
+        conditions = outcomes[heading]
+        cond_entropy = 0
+        for condition in conditions:
+            entropy = decision_trees.entropy(conditions[condition][0:2])
+            # print('{}: e={:.3f}'.format(condition, entropy))
+            cond_entropy += entropy * conditions[condition][2] / total_labels
+        # print('cond_entropy={:.3f} gain={:.3f}\n'.format(cond_entropy, entropy0 - cond_entropy))
+        gains[heading] = entropy0 - cond_entropy
+    return gains
+
+
+def get_zero_level_entropy(tennis_data):
+    labels = [getattr(tennis_row, 'play_tennis') for tennis_row in tennis_data]
+    probabilities = decision_trees.class_probabilities(labels)
+    return decision_trees.entropy(probabilities), len(labels)
+
 
 if __name__ == '__main__':
     tennis_data = get_tennis_data()
-
-    # starting entropy
-    tennis_play_labels = [tennis_row.play_tennis for tennis_row in tennis_data]
-    probabilities = decision_trees.class_probabilities(tennis_play_labels)
-    entropy_init = decision_trees.entropy(probabilities)
-    print(entropy_init)
-
-    # humidity entropy
-    entropy_hum = {}
-    total_len = 0
-    for hlevel in ['high', 'normal']:
-        labels = ([tennis_row.play_tennis for tennis_row in tennis_data
-                            if tennis_row.humidity == hlevel])
-        total_len += len(labels)
-        entropy_hum[hlevel] = (decision_trees.entropy(
-            decision_trees.class_probabilities(labels)), len(labels))
-    print(entropy_hum, total_len)
-
-    gain_hum = entropy_init
-    for hlevel in entropy_hum:
-        e, l = entropy_hum[hlevel]
-        gain_hum -= e * l / total_len
-    print(gain_hum)
-
+    print(get_first_level_gains(tennis_data))
